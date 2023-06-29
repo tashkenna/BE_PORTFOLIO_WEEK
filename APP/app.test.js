a86d7c459ab176f4f8b199f0bee48c48517b9266
Tash Kenna <kashtenna@tashs-mbp.home>
2023-06-29 15:16:50 +0100

    changed to update code

--- APP/app.test.js
+++ APP/app.test.js
@@ -152,9 +152,6 @@ describe("GET /app/articles/:article_id/comments", () => {
     return request(app)
       .get("/api/articles/7/comments")
       .expect(200)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Valid article ID, no comments found");
-      });
   })
  });
 
--- APP/model.js
+++ APP/model.js
@@ -70,10 +70,9 @@ WHERE
         return db
           .query("SELECT * FROM articles WHERE article_id = $1", [id])
           .then(({ rows }) => {
-            if (rows.length > 0) {
-            return Promise.reject({ status: 200, msg: "Valid article ID, no comments found" });;
+            if (rows.length === 0) {
+              return Promise.reject({ status: 404, msg: "Not found" });
             }
-            return Promise.reject({ status: 404, msg: "Not found" });
           });
       }
       return rows;
