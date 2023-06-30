9ffe5238dfd18704887bd21c5c2f09feccc55cad
tashkenna <natashakenna@gmail.com>
2023-06-30 15:48:48 +0100

    console log removed

--- APP/controller.js
+++ APP/controller.js
@@ -26,7 +26,6 @@ exports.getArticles = (req, res, next) => {
   const {topic, sort_by, order} = req.query
   selectArticles(topic, sort_by, order)
     .then((articles) => {
-      console.log(articles)
       res.status(200).send({ articles });
     })
     .catch((err) => {
